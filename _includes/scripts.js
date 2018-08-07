0a93186f2e9c792a3650a27dbe760ccd8d5649ac
stevenstetzler <steven.stetzler@gmail.com>
2018-08-07 12:25:17 -0400

    Made dropdowns close when you click anywhere

--- _includes/scripts.js
+++ _includes/scripts.js
@@ -1,35 +1,42 @@
-function toggleHeader(e) {
+function clearToggledHeaders() {
     /* Deactivate toggled elements */
     var toggled = document.querySelectorAll(".header_toggled");
-    var e_toggled = false;
     for (i = 0; i < toggled.length; i++) {
-      if (toggled[i] == e) {
-        e_toggled = toggled[i].className.includes("header_toggled");
-      }
-      toggled[i].className = toggled[i].className.replace(" header_toggled", "");
-      /* Hide child */
-      child = document.getElementById(toggled[i].id.replace("_parent", "_child"));
-      child.style.display = 'none';
+        toggled[i].className = toggled[i].className.replace(" header_toggled", "");
+        /* Hide child */
+        child = document.getElementById(toggled[i].id.replace("_parent", "_child"));
+        child.style.display = 'none';
     }
 
     /* Make all arrows down */
     var arrows = document.querySelectorAll(".arrow");
-    var my_arrow;
     for (i = 0; i < arrows.length; i++) {
-      if (arrows[i].parentElement.parentElement == e) {
-        my_arrow = arrows[i];
-      }
-      arrows[i].className = arrows[i].className.replace("up", "down");
+        arrows[i].className = arrows[i].className.replace("up", "down");
     }
+}
+
+function toggleHeader(e) {
+    var e_toggled = e.className.includes("header_toggled");
+    clearToggledHeaders();
 
     /* If selected header closed, open it */
     if (!e_toggled) {
-      /* Show child */
-      e.className += " header_toggled";
-      child = document.getElementById(e.id.replace("_parent", "_child"));
-      child.style.display = 'block';
-      child.style.height = 'auto';
-      /* Flip arrow */
-      my_arrow.className = my_arrow.className.replace("down", "up");
+        /* Show child */
+        e.className += " header_toggled";
+        child = document.getElementById(e.id.replace("_parent", "_child"));
+        child.style.display = 'block';
+        child.style.height = 'auto';
+        /* Flip arrow */
+        my_arrow.className = my_arrow.className.replace("down", "up");
     }
-  }
+}
+
+/* Click anywhere to close dropdowns */
+$('html').click(function() {
+    clearToggledHeaders()
+});
+
+/* If clicking on an actual dropdown, don't close */
+$('.header__item').click(function(event) {
+    event.stopPropagation();
+});
