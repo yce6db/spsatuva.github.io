e2e864a66ba38c51f19f6c1be0f61e5e3f6a89eb
stevenstetzler <steven.stetzler@gmail.com>
2018-08-11 15:46:49 -0400

    Re-organized js

--- _includes/scripts.js
+++ _includes/scripts.js
@@ -45,3 +45,21 @@ $('html').click(function() {
 $('.header__item').click(function(event) {
     event.stopPropagation();
 });
+
+$('.collapse-list-heading').click(function() {
+    var arrow_down = $(this).children('.arrow').attr('class').includes('down');
+    /* Flip arrows down of self and siblings in same list */
+    $(this).children('.arrow').removeClass('up down');
+    $(this).children('.arrow').addClass('down');
+    $(this).siblings('.collapse-list-heading').children('.arrow').removeClass('up down');
+    $(this).siblings('.collapse-list-heading').children('.arrow').addClass('down');
+    /* Flip own arrow if was already down */
+    if (arrow_down) {
+    $(this).children('.arrow').removeClass('up down');
+    $(this).children('.arrow').addClass('up');
+    }
+});
+
+$(document).ready(function() {
+    $("body").removeClass("preload");
+});
